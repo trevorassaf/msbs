30d7e81158bb0c35993dba59f14fe322c8a8ba55
Trevor Assaf <trevor.assaf@gmail.com>
2014-12-16 17:46:55 -0500

    moved mysql create/delete scripts

---
+++ projects/research_status/src/sql/create_tables.sql
@@ -0,0 +1,14 @@
+-- Create Tables for MSBS Research Schedule --
+
+source create_tables/create_sexes.sql
+source create_tables/create_patients.sql
+
+source create_tables/create_regions.sql
+source create_tables/create_surgeons.sql
+source create_tables/create_complications.sql
+source create_tables/create_surgeries.sql
+
+source create_tables/create_packet_data_stores.sql
+source create_tables/create_intervals.sql
+source create_tables/create_interviews.sql
+source create_tables/create_interview_calls.sql
